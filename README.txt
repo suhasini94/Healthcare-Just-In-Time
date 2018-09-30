a0098ea346cf7b6315fd28129c0f0c4b3ceb5211
suhasini94 <prosuha@gmail.com>
2018-09-30 22:08:31 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+The following file contains instructions for running this application:
+
+If using MacOS, download MAMP and launch the Apache web server. Edit 
+the "Preferences" for MAMP in order to add this folder as the root folder.
+localhost port 8888 at http://localhost:8888/ is now ready to view the
+"Healthcare, Just In Time" application. If using Windows, download XAMPP instead
+and launch an Apache web server. Also, ensure this folder is the root folder. 
+Search for the String "Listen" to find out the port XAMPP uses and then
+launch localhost at that port number in order to view the application. 
---
+++ index.php
@@ -0,0 +1,81 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+/**
+ * ApiMedic.com by praid: https://apimedic.com
+ * This is free implementation: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License Version 3 as published by the Free Software Foundation.
+ * 
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with ApiMedic.com. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Modified by Suhasini Suresh 
+ */
+ -->
+<head>
+    <title>HEALTHCARE, JUST IN TIME</title>
+    <link rel="stylesheet" type="text/css" href="symptom_selector/selector.css">
+    <link rel="stylesheet" type="text/css" href="symptom_selector/fontawesome/assets/css/font-awesome.min.css" />
+    <script src="libs/jquery-1.12.2.min.js"></script>
+    <script src="libs/json2.js"></script><!-- json for ie7 -->
+    <script src="libs/jquery.imagemapster.min.js?v=1.2.10"></script>
+    <script src="libs/typeahead.bundle.js"></script>
+    
+    <script src="symptom_selector/selector.js"></script>
+
+    <h1 class="title" style="text-align:center; color: #525252; font-size: 300%">HEALTHCARE, JUST IN TIME</h1>
+
+    <!--unique user token for every session; need to have APIMedic account for dev purposes-->
+	<?php 
+	
+	if ( !isset( $_SESSION['userToken']) || !isset( $_SESSION['tokenExpireTime']) || time() >= $_SESSION['tokenExpireTime'] )
+	{
+		require 'token_generator.php';
+		$tokenGenerator = new TokenGenerator("suredr@yahoo.com","Me53KdNz2c6PWp8x9","https://sandbox-authservice.priaid.ch/login");
+		$token = $tokenGenerator->loadToken();
+		$_SESSION['userToken'] = $token->{'Token'};
+		$_SESSION['tokenExpireTime'] = time() + $token->{'ValidThrough'};
+	}
+
+	$token = $_SESSION['userToken'];
+	?>
+
+	<script type="text/javascript">
+
+		var userToken = <?php echo "'".$token."'" ?>;
+		
+        $(document).ready(function () {
+            $("#symptomSelector").symptomSelector(
+            {
+                mode: "diagnosis",
+                webservice: "https://sandbox-healthservice.priaid.ch",
+                language: "en-gb",
+                specUrl: "sample_specialisation_page",
+                accessToken: userToken
+            });
+        });
+    </script>	
+</head>
+
+<!-- Symptom checker body -->
+<body>
+
+    <table class="container-table">
+        <tr>
+            <td valign="middle" colspan="2" class="td-header box-white bordered-box width50"><h4 class="header" id="selectSymptomsTitle"><span class="badge pull-left badge-primary visible-lg margin5R">1</span></h4></td>
+            <td valign="middle" class="td-header bordered-box box-white width25"><h4 class="header" id="selectedSymptomsTitle"><span class="badge pull-left badge-primary visible-lg margin5R">2</span></h4></td>
+            <td valign="middle" class="td-header bordered-box box-white width25"><h4 class="header" id="possibleDiseasesTitle"><span class="badge pull-left badge-primary visible-lg margin5R">3</span></h4></td>
+        </tr>
+        <tr>
+            <td valign="top" class="selector_container bordered-box box-white width25"><div id="symptomSelector"></div></td>
+            <td valign="top" class="selector_container bordered-box box-white width25"><div id="symptomList"></div></td>
+            <td valign="top" class="selector_container bordered-box box-white width25"><div id="selectedSymptomList"></div></td>
+            <td valign="top" class="selector_container bordered-box box-white width25"><div id="diagnosisList"></div></td>
+        </tr>
+    </table>
+    <div>
+        <a target="_blank" href="http://apimedic.com"><img class="logo" alt="ApiMedic by priaid" src="symptom_selector/images/logo.jpg" /></a>
+        <span ><a class="priaid-powered" target="_blank" href="http://apimedic.com"> powered by  </a> </span>
+    </div>
+</body>
+</html>
